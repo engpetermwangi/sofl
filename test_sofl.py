eaa798b558f7c677da089db4a72458581a3e6920
Mwangi <pmwangi155@gmail.com>
2019-01-26 22:58:35 +0300

    authentication tests set, refactor code to pass

--- test_sofl.py
+++ test_sofl.py
@@ -8,5 +8,32 @@ def test_client_works():
     response = client.get('/nonexistent_endpoint')
     assert response.status_code == 404
 
+def signup(client, username, password):
+    return client.post('/auth/signup', data = {
+        'username': username,
+        'password': password
+        }, follow_redirects = True)
+
+def login(client, username, password):
+    return client.post('/login', data = {
+        'username': username,
+        'password', password
+        }, follow_redirects = True)
+
+def logout(client):
+    return client.post('/logout', follow_redirects = True)
+
+def test_signup(client):
+    response = signup(client, app.config['USERNAME'], app.config['PASSWORD'])
+    assert 'Successful signup!' in response.data
+
+def test_login(clinet):
+    response = login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    assert 'Successful login!' in response.data
+
+def test_logout(client):
+    response = logout(client)
+    assert 'Successful logout!' in response.data
+
 if __name__ == '__main__':
 	pytest.main()
